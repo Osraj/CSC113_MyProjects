b1983d07f6f155dbfeef3a8d964cc70e75f58c84
Osamah Sarraj <osamah.sarraj@gmail.com>
2022-12-19 10:54:51 +0300

    Adding AudioBook as inheritance class from Book.java

--- Lab/Lab04/src/Book.java
+++ Lab/Lab04/src/Book.java
@@ -2,6 +2,11 @@ public class Book {
     protected String title;
     protected String author;
 
+    public Book(){
+        title = "NoTitle";
+        author = "NoAuthor";
+    }
+
     public Book(String title, String author){
         this.title = title;
         this.author = author;
