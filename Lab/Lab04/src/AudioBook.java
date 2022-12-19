87a863e5d9effb14f24874593db490da75a83cf9
Osamah Sarraj <osamah.sarraj@gmail.com>
2022-12-19 10:57:43 +0300

    Adding the Basic methods to AudioBook.java

--- Lab/Lab04/src/AudioBook.java
+++ Lab/Lab04/src/AudioBook.java
@@ -1,2 +1,20 @@
 public class AudioBook extends Book {
+    private int duration;
+    private String narrator;
+
+    public AudioBook(String title, String author, int duration, String narrator){
+        this.title = title;
+        this.author = author;
+        this.duration = duration;
+        this.narrator = narrator;
+    }
+
+    public int getDuration(){
+        return duration;
+    }
+
+    public String getNarrator(){
+        return narrator;
+    }
+
 }
