8ca30e843585fd9063d92273c03c759594c84725
Osamah Sarraj <osamah.sarraj@gmail.com>
2023-01-09 11:02:02 +0300

    Create Employee.java

---
+++ Lab/Lab09/src/Employee.java
@@ -0,0 +1,29 @@
+// Osamah Sarraj, 443101217, Lab09
+public abstract class Employee {
+    private String name;
+    private int id;
+
+    public Employee(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public Employee(Employee e){
+        name = e.name;
+        id = e.id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void display(){
+        System.out.print("Name: " + name + ", ID: " + id);
+    }
+
+    public abstract double calculatePay();
+}
