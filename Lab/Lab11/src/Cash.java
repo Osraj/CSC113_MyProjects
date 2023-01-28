3cd1a73ce1e937dd625b5c54c43aa7903bb6be60
Osamah Sarraj <osamah.sarraj@gmail.com>
2023-01-28 10:31:23 +0300

    Create Cash.java

---
+++ Lab/Lab11/src/Cash.java
@@ -0,0 +1,17 @@
+public class Cash extends Donation {
+    private String currency;
+
+    public Cash(double amount, String location, String donorName, String currency){
+        super(amount, location, donorName);
+        this.currency = currency;
+    }
+
+    public String getCurrency(){
+        return currency;
+    }
+
+    public void display(){
+        super.display();
+        System.out.println("Currency: " + currency);
+    }
+}
