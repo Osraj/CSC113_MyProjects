cee3bcf3025717c4ed201a596c5100aeb2edd918
Osamah Sarraj <osamah.sarraj@gmail.com>
2023-01-28 10:31:17 +0300

    Create Donation.java

---
+++ Lab/Lab11/src/Donation.java
@@ -0,0 +1,25 @@
+public class Donation {
+    private double amount;
+    private String location;
+    private String donorName;
+
+    public Donation(double amount, String location, String donorName){
+        this.amount = amount;
+        this.location = location;
+        this.donorName = donorName;
+    }
+
+    public String getDonorName(){
+        return donorName;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public void display(){
+        System.out.println("Donor Name: " + donorName);
+        System.out.println("Amount: " + amount);
+        System.out.println("Location: " + location);
+    }
+}
